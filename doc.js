cf21d57844e592a4000c7bdfc5e6b26619effd52
GNANA MOORTHY D <129540732+GnanaMoorthy06@users.noreply.github.com>
2025-12-11 22:10:24 +0530

    router setup done

--- doc.js
+++ doc.js
@@ -65,6 +65,8 @@ go to terminal : npm install dotenv
 create .env file => PORT=5000
 go to index.js file and write the code below
 
+code start of index.js file --------------------
+
 import express from 'express';
 
 import dotenv from 'dotenv/config';
@@ -81,16 +83,63 @@ app.listen(PORT, () => {
     console.log('\x1b[31m%s\x1b[0m',`Server running on port ${PORT}`);
 });
 
+code end of index.js file --------------------
+
 go to terminal : npm run dev
 now you can see the Server running on port ${PORT} message in terminal
 open postman software => GET method => http://localhost:5000/api/check => send
 you can see the message in postman => { "message": "API is running..." }
 
 
+Express router
+----------------
+create a folder in src named routes
+create router.js file in routes folder and write the code below
 
+start of router.js file code --------------------
 
+import Express from 'express';
+const router = Express.Router();
 
+router.route('/api/getContacts').get((req, res) => {
+    res.status(200).json({ message: `Get all contacts` });
+});
 
+router.route('/api/getContact/:id').get((req, res) => {
+    res.status(200).json({ message: `Get all contact ${req.params.id}` });
+});
+
+router.route('/api/createContact').post((req, res) => {
+    res.status(200).json({ message: `New contact created` });
+});
+
+router.route('/api/updateContact/:id').put((req, res) => {
+    res.status(200).json({ message: `Contact updated in ${req.params.id}` });
+});
+
+router.route('/api/deleteContact/:id').delete((req, res) => {
+    res.status(200).json({ message: `Contact delete in ${req.params.id}` });
+});
+
+export default router;
+
+end of router.js file code --------------------
+
+go to index.js file and import the router file and use the router middleware
+
+like below
+
+import router from './routes/router.js';
+
+app.use('/', router);
+
+now you can test all the api endpoints in postman
+
+get all contact     :    GET                   :      /api/getContacts
+get  contact        :    GET                   :      /api/getContact/:id
+create contact      :    POST                  :      /api/createContact
+update contact      :    PUT                   :      /api/updateContact/:id
+delete contact      :    DELETE                :      /api/deleteContact/:id
 
 
 **/
